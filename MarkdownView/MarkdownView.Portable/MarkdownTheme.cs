821a8bdfb8aab153687b97213d492744d6007b3b
Aloïs Deniel <alois.deniel@gmail.com>
2017-08-30 00:04:38 +0200

    Added dark theme.

--- MarkdownView/MarkdownView.Portable/MarkdownTheme.cs
+++ MarkdownView/MarkdownView.Portable/MarkdownTheme.cs
@@ -5,6 +5,91 @@ namespace Markdown
 
     public class MarkdownTheme
     {
+        public MarkdownTheme()
+        {
+            this.Paragraph = new MarkdownStyle
+            {
+                Attributes = FontAttributes.None,
+                FontSize = 12,
+            };
+
+            this.Heading1 = new MarkdownStyle
+            {
+                Attributes = FontAttributes.Bold,
+                BorderSize = 1,
+                FontSize = 26,
+            };
+
+            this.Heading2 = new MarkdownStyle
+            {
+                Attributes = FontAttributes.Bold,
+                BorderSize = 1,
+                FontSize = 22,
+            };
+
+            this.Heading3 = new MarkdownStyle
+            {
+                Attributes = FontAttributes.Bold,
+                FontSize = 20,
+            };
+
+            this.Heading4 = new MarkdownStyle
+            {
+                Attributes = FontAttributes.Bold,
+                FontSize = 18,
+            };
+
+            this.Heading5 = new MarkdownStyle
+            {
+                Attributes = FontAttributes.Bold,
+                FontSize = 16,
+            };
+
+            this.Heading6 = new MarkdownStyle
+            {
+                Attributes = FontAttributes.Bold,
+                FontSize = 14,
+            };
+
+            this.Link = new MarkdownStyle
+            {
+                Attributes = FontAttributes.None,
+                FontSize = 12,
+            };
+
+            this.Code = new MarkdownStyle
+            {
+                Attributes = FontAttributes.None,
+                FontSize = 12,
+            };
+
+            this.Quote = new MarkdownStyle
+            {
+                Attributes = FontAttributes.None,
+                BorderSize = 4,
+                FontSize = 12,
+            };
+
+            this.Separator = new MarkdownStyle
+            {
+                BorderSize = 2,
+            };
+
+            // Platform specific properties
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    Code.FontFamily = "Courier";
+                    break;
+
+                case Device.Android:
+                    Code.FontFamily = "monospace";
+                    break;
+            }
+        }
+
+        public Color BackgroundColor { get; set; }
+
         public MarkdownStyle Paragraph { get; set; } 
 
         public MarkdownStyle Heading1 { get; set; } 
@@ -26,107 +111,31 @@ namespace Markdown
         public MarkdownStyle Link { get; set; }
 
         public MarkdownStyle Code { get; set; } 
-
     }
 
     public class LightMarkdownTheme : MarkdownTheme
     {
         public LightMarkdownTheme()
         {
-            this.Paragraph = new MarkdownStyle
-            {
-                Attributes = FontAttributes.None,
-                ForegroundColor = DefaultTextColor,
-                FontSize = 12,
-            };
-
-            this.Heading1 = new MarkdownStyle
-            {
-                Attributes = FontAttributes.Bold,
-                ForegroundColor = DefaultTextColor,
-                BorderSize = 1,
-                BorderColor = DefaultSeparatorColor,
-                FontSize = 26,
-            };
-
-            this.Heading2 = new MarkdownStyle
-            {
-                Attributes = FontAttributes.Bold,
-                ForegroundColor = DefaultTextColor,
-                BorderSize = 1,
-                BorderColor = DefaultSeparatorColor,
-                FontSize = 22,
-            };
-
-            this.Heading3 = new MarkdownStyle
-            {
-                Attributes = FontAttributes.Bold,
-                ForegroundColor = DefaultTextColor,
-                FontSize = 20,
-            };
-
-            this.Heading4 = new MarkdownStyle
-            {
-                Attributes = FontAttributes.Bold,
-                ForegroundColor = DefaultTextColor,
-                FontSize = 18,
-            };
-
-            this.Heading5 = new MarkdownStyle
-            {
-                Attributes = FontAttributes.Bold,
-                ForegroundColor = DefaultTextColor,
-                FontSize = 16,
-            };
-
-            this.Heading6 = new MarkdownStyle
-            {
-                Attributes = FontAttributes.Bold,
-                ForegroundColor = DefaultTextColor,
-                FontSize = 14,
-            };
-
-            this.Link = new MarkdownStyle
-            {
-                Attributes = FontAttributes.None,
-                ForegroundColor = DefaultAccentColor,
-                FontSize = 12,
-            };
-
-            this.Code = new MarkdownStyle
-            {
-                Attributes = FontAttributes.None,
-                BackgroundColor = DefaultCodeBackground,
-                FontSize = 12,
-            };
-
-            this.Quote = new MarkdownStyle
-            {
-                Attributes = FontAttributes.None,
-                ForegroundColor = DefaultQuoteTextColor,
-                BorderSize = 4,
-                BorderColor = DefaultQuoteBorderColor,
-                FontSize = 12,
-            };
-
-            this.Separator = new MarkdownStyle
-            {
-                BorderSize = 2,
-                BorderColor = DefaultSeparatorColor,
-            };
-
-            // Platform specific properties
-            switch (Device.RuntimePlatform)
-            {
-                case Device.iOS:
-                    Code.FontFamily = "Courier";
-                    break;
-
-                case Device.Android:
-                    Code.FontFamily = "monospace";
-                    break;
-            }
+            this.BackgroundColor = DefaultBackgroundColor;
+            this.Paragraph.ForegroundColor = DefaultTextColor;
+            this.Heading1.ForegroundColor = DefaultTextColor;
+            this.Heading1.BorderColor = DefaultSeparatorColor;
+            this.Heading2.ForegroundColor = DefaultTextColor;
+            this.Heading2.BorderColor = DefaultSeparatorColor;
+            this.Heading3.ForegroundColor = DefaultTextColor;
+            this.Heading4.ForegroundColor = DefaultTextColor;
+            this.Heading5.ForegroundColor = DefaultTextColor;
+            this.Heading6.ForegroundColor = DefaultTextColor;
+            this.Link.ForegroundColor = DefaultAccentColor;
+            this.Code.ForegroundColor = DefaultTextColor;
+            this.Code.BackgroundColor = DefaultCodeBackground;
+            this.Quote.ForegroundColor = DefaultQuoteTextColor;
+            this.Quote.BorderColor = DefaultQuoteBorderColor;
+            this.Separator.BorderColor = DefaultSeparatorColor;
         }
+
+        public static readonly Color DefaultBackgroundColor = Color.FromHex("#ffffff");
 
         public static readonly Color DefaultAccentColor = Color.FromHex("#0366d6");
 
@@ -139,5 +148,42 @@ namespace Markdown
         public static readonly Color DefaultQuoteTextColor = Color.FromHex("#6a737d");
 
         public static readonly Color DefaultQuoteBorderColor = Color.FromHex("#dfe2e5");
+    }
+
+    public class DarkMarkdownTheme : MarkdownTheme
+    {
+        public DarkMarkdownTheme()
+        {
+            this.BackgroundColor = DefaultBackgroundColor;
+            this.Paragraph.ForegroundColor = DefaultTextColor;
+            this.Heading1.ForegroundColor = DefaultTextColor;
+            this.Heading1.BorderColor = DefaultSeparatorColor;
+            this.Heading2.ForegroundColor = DefaultTextColor;
+            this.Heading2.BorderColor = DefaultSeparatorColor;
+            this.Heading3.ForegroundColor = DefaultTextColor;
+            this.Heading4.ForegroundColor = DefaultTextColor;
+            this.Heading5.ForegroundColor = DefaultTextColor;
+            this.Heading6.ForegroundColor = DefaultTextColor;
+            this.Link.ForegroundColor = DefaultAccentColor;
+            this.Code.ForegroundColor = DefaultTextColor;
+            this.Code.BackgroundColor = DefaultCodeBackground;
+            this.Quote.ForegroundColor = DefaultQuoteTextColor;
+            this.Quote.BorderColor = DefaultQuoteBorderColor;
+            this.Separator.BorderColor = DefaultSeparatorColor;
+        }
+
+        public static readonly Color DefaultBackgroundColor = Color.FromHex("#2b303b");
+
+        public static readonly Color DefaultAccentColor = Color.FromHex("#d08770");
+
+        public static readonly Color DefaultTextColor = Color.FromHex("#eff1f5");
+
+        public static readonly Color DefaultCodeBackground = Color.FromHex("#4f5b66");
+
+        public static readonly Color DefaultSeparatorColor = Color.FromHex("#65737e");
+
+        public static readonly Color DefaultQuoteTextColor = Color.FromHex("#a7adba");
+
+        public static readonly Color DefaultQuoteBorderColor = Color.FromHex("#a7adba");
     }
 }
